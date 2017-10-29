b18b88a8c981272d1208bc04c0734aeacc553f51
Dizzy <itsdizzy40@gmail.com>
2017-10-29 16:51:28 +0100

    Readded this

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'
 echo ":/home/container$ yarn && ${MODIFIED_STARTUP}"
 
 # Always run yarn at startup, this will make sure node_modules are up to date
-# yarn
+yarn
 
 # Run the Server
 ${MODIFIED_STARTUP}
