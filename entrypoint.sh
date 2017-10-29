d222f0f7ba780ea74e235bcd52b58e28dfac46ab
Dizzy <itsdizzy40@gmail.com>
2017-10-29 16:34:15 +0100

    attempt 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,10 @@ cd /home/container
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
+echo ":/home/container$ yarn && ${MODIFIED_STARTUP}"
 
 # Always run yarn at startup, this will make sure node_modules are up to date
-yarn
+# yarn
 
 # Run the Server
 ${MODIFIED_STARTUP}
